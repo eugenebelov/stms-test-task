cfbd67138173367511b43a9698c6e6446f1ab09f
Eugene Belov <eugene.belov@smashdocs.net>
2018-04-08 01:28:13 +0300

    update guard

--- src/app/authentication-guard.service.ts
+++ src/app/authentication-guard.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 
 import { LoginService } from "./login/login.service";
 
@@ -8,7 +9,10 @@ export class AuthenticationGuard implements CanActivate {
 
   constructor(private router: Router, private login: LoginService) {}
 
-  canActivate(): boolean {
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+
     console.log("Auth Guard", this.login.loggedIn());
 
     if(this.login.loggedIn()) {
